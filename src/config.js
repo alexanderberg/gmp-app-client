0a0bab9d68be0f295f7f39a9623fd4c3ecd04f40
alexanderberg <alex@crowdvalley.com>
2018-11-30 17:54:54 +0000

    Configuring environments

--- src/config.js
+++ src/config.js
@@ -1,17 +1,44 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
+    REGION: "eu-west-2",
+    BUCKET: "gmp-app-api-2-dev-attachmentsbucket-1cx5d41rg6b87"
   },
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
+    REGION: "eu-west-2",
+    URL: "https://api.vanillaslender.com/dev"
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_wPC1P6UfN",
+    APP_CLIENT_ID: "53fg6f4drpiv3ejh5hgdpj60g0",
+    IDENTITY_POOL_ID: "eu-west-2:7c57622d-e429-4863-940a-c1cb32536ab7"
   }
+};
+
+const prod = {
+  s3: {
+    REGION: "eu-west-2",
+    BUCKET: "gmp-app-api-2-prod-attachmentsbucket-wgy2ivzqh2fx"
+  },
+  apiGateway: {
+    REGION: "eu-west-2",
+    URL: "https://api.vanillaslender.com/prod"
+  },
+  cognito: {
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_jBJumizVl",
+    APP_CLIENT_ID: "65juetjbe653pjc9tu0e62it1s",
+    IDENTITY_POOL_ID: "eu-west-2:5926c386-e54c-47c2-8222-d806ec58daf1"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
